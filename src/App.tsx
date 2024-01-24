85e77173bf562f2360f154c6481030a06b89d65a
Divineridh <98535900+Divineridh@users.noreply.github.com>
2024-01-24 18:22:46 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+import Layout from './components/Layout'
+import DashboardPage from './pages/DashboardPage'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { NextUIProvider } from '@nextui-org/react'
+
+function App() {
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex flex-col">
+      <NextUIProvider navigate={navigate}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<DashboardPage />} />
+          </Route>
+        </Routes>
+      </NextUIProvider>
+    </div>
+  )
+}
+
+export default App
