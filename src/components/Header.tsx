f59e6c86f670debdddf0ab28134ce67c848b7c5d
Divineridh <98535900+Divineridh@users.noreply.github.com>
2024-01-25 02:16:35 -0300

    some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Image } from '@nextui-org/react'
+import Leon from '../assets/leonGlasses.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="flex flex-col bg-slate-800 text-gray-200">
+        <div className="flex flex-row items-center justify-around p-2 border-b border-slate-700">
+            <Image width={30} src={Leon} />
+            <span className="text-lg">Todo App</span>
+            <span className="">Nachito-InProgress</span>
+        </div>
+        <div className="flex flex-row justify-center p-3 shadow-lg">Eventually tabs</div>
+    </div>
   )
 }
