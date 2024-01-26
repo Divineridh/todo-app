d1d89bf1488ec37fd941fc8c7bf650a5bb3aacbb
Divineridh <98535900+Divineridh@users.noreply.github.com>
2024-01-26 15:05:37 -0300

    potential deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      }
+    },
     base: '/',
   }
 
